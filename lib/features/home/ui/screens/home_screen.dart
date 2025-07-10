7a53ab0643a213cbbfc0709bc836b9c4fd135f1c
SanaulParvej <perves15-6433@s.diu.edu.bd>
2025-07-11 01:34:57 +0600

    [WIP] home screen ui design in progress

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:crafty_bay/app/asset_paths.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../../../auth/ui/widgets/app_bar_icon_button.dart';
+import '../../../auth/ui/widgets/home_carousel_slider.dart';
 import '../../../auth/ui/widgets/product_search_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -25,6 +26,25 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               SizedBox(height: 16),
               ProductSearchBar(),
+              SizedBox(height: 16),
+              HomeCarouselSlider(),
+              SizedBox(height: 16),
+              _buildSectionHeader(
+                title: 'Categories',
+                onTapSeeAll: () {},
+              ),
+              _buildSectionHeader(
+                title: 'Popular',
+                onTapSeeAll: () {},
+              ),
+              _buildSectionHeader(
+                title: 'Special',
+                onTapSeeAll: () {},
+              ),
+              _buildSectionHeader(
+                title: 'New ',
+                onTapSeeAll: () {},
+              ),
             ],
           ),
         ),
@@ -39,6 +59,17 @@ class _HomeScreenState extends State<HomeScreen> {
         AppBarIconButton(iconData: Icons.person, onTap: () {}),
         AppBarIconButton(iconData: Icons.call, onTap: () {}),
         AppBarIconButton(iconData: Icons.notifications, onTap: () {}),
+      ],
+    );
+  }
+
+  Widget _buildSectionHeader(
+      {required String title, required VoidCallback onTapSeeAll}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(title, style: Theme.of(context).textTheme.titleSmall),
+        TextButton(onPressed: onTapSeeAll, child: Text('Sell all'))
       ],
     );
   }
